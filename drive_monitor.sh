fa9dab89446682a42d52b427e9e35eb6cbf8bc0b
Aayush Jain <imjainaayush@gmail.com>
2020-09-14 17:20:17 +0530

    Updated Script

--- drive_monitor.sh
+++ drive_monitor.sh
@@ -1,11 +1,11 @@
-path="/home/aayush/criodo/Google-Drive"  
-while true 
+path="/home/aayush/criodo/Google-Drive"
+while true
 do
 	inotifywait --recursive -qq -e attrib,create,delete,modify,delete_self,move,move_self,close_write $path
-	cd $path                              > /dev/null &> /dev/null
-	git add --all                         > /dev/null &> /dev/null
-	now=$(date)                           > /dev/null &> /dev/null
-	git commit -m "Auto-Commit at : $now" > /dev/null &> /dev/null
-	git pull                              > /dev/null &> /dev/null
-	git push -u origin master             > /dev/null &> /dev/null
+	cd $path
+	git add --all
+	now=$(date)
+	git commit -m "Auto-Commit at : $now"
+	git pull
+	git push -u origin master
 done
